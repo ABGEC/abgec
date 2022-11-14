a36f80eef3070c7fe25ab1964c32253bf898ea45
arwazkhan189 <arwazkhan189@gmail.com>
2022-11-14 19:09:25 +0530

    update

--- assets/scripts/team.js
+++ assets/scripts/team.js
@@ -16,7 +16,7 @@ const swiper = new Swiper('.mySwiper', {
   breakpoints: {
     // when window width is >= 320px
     320: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 20
     },
     // when window width is >= 480px
